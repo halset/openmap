8534ee57c0308144ddb4b3d1a6ca1a2010baa8c3
Tore Halset <halset@pvv.ntnu.no>
2012-07-30 13:02:17 +0200

    use ',' between feature info outputs from multiple layers if the format is json

--- src/openmap/com/bbn/openmap/image/wms/DefaultFeatureInfoResponse.java
+++ src/openmap/com/bbn/openmap/image/wms/DefaultFeatureInfoResponse.java
@@ -1,63 +1,89 @@
 package com.bbn.openmap.image.wms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import com.bbn.openmap.util.http.HttpConnection;
 
-public class DefaultFeatureInfoResponse implements FeatureInfoResponse {
+public class DefaultFeatureInfoResponse
+      implements FeatureInfoResponse {
 
-	private StringBuffer out;
-	private String contentType;
+   private StringBuffer out;
+   private String contentType;
 
-	public void setOutput(String contentType, StringBuffer out) {
-		this.out = out;
-		this.contentType = contentType;
+   private final List<StringBuffer> parts = new ArrayList<StringBuffer>();
 
-		appendHeader();
-	}
+   public void setOutput(String contentType, StringBuffer out) {
+      this.out = out;
+      this.contentType = contentType;
 
-	public void flush() {
-		appendFooter();
-	}
+      appendHeader();
+   }
 
-	public Collection<String> getInfoFormats() {
-		return Arrays.asList(HttpConnection.CONTENT_HTML,
-				HttpConnection.CONTENT_PLAIN);
-	}
+   public void flush() {
+      for (Iterator<StringBuffer> it = parts.iterator(); it.hasNext();) {
+         write(it.next());
+         if (it.hasNext()) {
+            appendSeparator();
+         }
+      }
+      appendFooter();
+   }
 
-	public void output(LayerFeatureInfoResponse layerFeatureInfoResponse) {
-		layerFeatureInfoResponse.output(contentType, out);
-	}
-	
-	protected void write(String s){
-		out.append(s);
-	}
-	
-	protected String getContentType(){
-		return contentType;
-	}
+   public Collection<String> getInfoFormats() {
+      return Arrays.asList(HttpConnection.CONTENT_HTML, HttpConnection.CONTENT_PLAIN);
+   }
 
-	protected void appendHeader() {
-		if (getContentType().equals(HttpConnection.CONTENT_HTML)) {
-			write("<html><head>\n");
-			write("<meta http-equiv=\"content-type\"\n");
-			write("      content=\"text/html; charset=UTF-8\">\n");
-			write("</head><body>\n");
-		} else if (getContentType().equals(HttpConnection.CONTENT_JSON)) {
-			write("{\n");
-			write("  \"type\": \"FeatureCollection\",\n");
-			write("  \"features\": [\n");
-		}
-	}
+   public void output(LayerFeatureInfoResponse layerFeatureInfoResponse) {
+      // store in local buffer and collect all the parts. important for formats
+      // with separators between matches like (geo)json.
+      StringBuffer part = new StringBuffer();
+      layerFeatureInfoResponse.output(getContentType(), part);
+      if (part.length() > 0) {
+         parts.add(part);
+      }
+   }
 
-	protected void appendFooter() {
-		if (getContentType().equals(HttpConnection.CONTENT_HTML)) {
-			write("</body></html>");
-		} else if (getContentType().equals(HttpConnection.CONTENT_JSON)) {
-			write("  ]\n");
-			write("}\n");
-		}
-	}
+   protected void write(CharSequence s) {
+      out.append(s);
+   }
+   
+   protected String getContentType() {
+      return contentType;
+   }
+
+   protected void appendHeader() {
+      if (getContentType().equals(HttpConnection.CONTENT_HTML)) {
+         write("<html><head>\n");
+         write("<meta http-equiv=\"content-type\"\n");
+         write("      content=\"text/html; charset=UTF-8\">\n");
+         write("</head><body>\n");
+      } else if (getContentType().equals(HttpConnection.CONTENT_JSON)) {
+         write("{\n");
+         write("  \"type\": \"FeatureCollection\",\n");
+         write("  \"features\": [\n");
+      }
+   }
+
+   protected void appendFooter() {
+      if (getContentType().equals(HttpConnection.CONTENT_HTML)) {
+         write("</body></html>");
+      } else if (getContentType().equals(HttpConnection.CONTENT_JSON)) {
+         write("  ]\n");
+         write("}\n");
+      }
+   }
+
+   /**
+    * Append the separator to be used between output of two {@link LayerFeatureInfoResponse}s
+    */
+   protected void appendSeparator() {
+      if (getContentType().equals(HttpConnection.CONTENT_JSON)) {
+         write(",\n");
+      }
+   }
 
 }
