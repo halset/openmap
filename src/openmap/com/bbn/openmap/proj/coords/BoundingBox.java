013bc2092b5d4e9bc1bafa77378254599f137ee5
halset <halset@ecc.no>
2012-11-11 21:09:29 +0100

    added code to convert wms bbox to BoundingBox and back

--- src/openmap/com/bbn/openmap/proj/coords/BoundingBox.java
+++ src/openmap/com/bbn/openmap/proj/coords/BoundingBox.java
@@ -1,5 +1,6 @@
 package com.bbn.openmap.proj.coords;
 
+
 /**
  * A Bounding Box.
  */
@@ -35,5 +36,31 @@ public class BoundingBox {
     public double getMaxY() {
         return maxy;
     }
+    
+    public static BoundingBox fromString(String bbox) {
+       if (bbox == null) {
+           throw new IllegalArgumentException("Missing BBOX parameter");
+       }
+
+       String[] arrayBBox = bbox.split(",");
+
+       if (arrayBBox.length != 4) {
+           throw new IllegalArgumentException(
+                   "Invalid BBOX parameter. BBOX must contain exactly 4 values separated with comas.");
+       }
+
+       double minX = Double.parseDouble(arrayBBox[0]);
+       double minY = Double.parseDouble(arrayBBox[1]);
+       double maxX = Double.parseDouble(arrayBBox[2]);
+       double maxY = Double.parseDouble(arrayBBox[3]);
+
+       return new BoundingBox(minX, minY, maxX, maxY);
+   }
+    
+   @Override
+   public String toString() {
+      return getMinX() + "," + getMinY() + "," + getMaxX() + "," + getMaxY();
+   }
+
 
 }
