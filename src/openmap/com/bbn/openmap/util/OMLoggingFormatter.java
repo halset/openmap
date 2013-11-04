65ab0fc0b60300acfcfc0d0761be997ea9192ffb
halset <halset@ecc.no>
2013-11-04 15:27:43 +0100

    skip AccessController.doPrivileged

--- src/openmap/com/bbn/openmap/util/OMLoggingFormatter.java
+++ src/openmap/com/bbn/openmap/util/OMLoggingFormatter.java
@@ -34,7 +34,7 @@ public class OMLoggingFormatter extends Formatter {
  
     // Line separator string. This is the value of the line.separator
     // property at the moment that the SimpleFormatter was created.
-    private String lineSeparator = (String) java.security.AccessController.doPrivileged(new sun.security.action.GetPropertyAction("line.separator"));
+    private String lineSeparator = System.getProperty("line.separator");
 
     /**
      * Format the given LogRecord.
